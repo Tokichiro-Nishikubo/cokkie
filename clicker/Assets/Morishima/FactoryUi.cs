c3c7bc0f9b6ec8b2bdca73bde49aa6664a344b1c
森島裕介 <yusuke.pon2003@gmail.com>
2025-06-28 15:13:35 +0900

    TMP適用

--- clicker/Assets/Morishima/FactoryUi.cs
+++ clicker/Assets/Morishima/FactoryUi.cs
@@ -1,32 +1,132 @@
+using naichilab.Scripts.Extensions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 //using UnityEngine.UIElements;
 
 public class FactoryUi : MonoBehaviour
 {
-    public Text buttonName;
-    public Text buttonCost;
-    public Text buttonLevel;
+    AddFactoryBase factoryBase;
+    public enum ButtonType
+    {
+        Point,
+        Amount,
+        Speed
+    }
+    public ButtonType buttonType;
+    public TextMeshProUGUI buttonName;
+    public TextMeshProUGUI buttonCost;
+    public TextMeshProUGUI buttonLevel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        factoryBase = GameManager.Instance.GetFactory();
+        if (buttonName != null)
+        {
+            switch (buttonType)
+            {
+                case ButtonType.Point:
+                    buttonName.text = "ポイント";
+                    break;
+                case ButtonType.Amount:
+                    buttonName.text = "量";
+                    break;
+                case ButtonType.Speed:
+                    buttonName.text = "速度";
+                    break;
+            }
+        }
+        if (buttonCost != null)
+        {
+            switch (buttonType)
+            {
+                case ButtonType.Point:
+                    buttonCost.text = factoryBase.GetPointCost().ToReadableString();
+                    break;
+                case ButtonType.Amount:
+                    buttonCost.text = factoryBase.GetAmountCost().ToReadableString();
+                    break;
+                case ButtonType.Speed:
+                    buttonCost.text = factoryBase.GetSpeedCost().ToReadableString();
+                    break;
+            }
+        }
+        if (buttonLevel != null)
+        {
+            switch (buttonType)
+            {
+                case ButtonType.Point:
+                    buttonLevel.text = factoryBase.PointNum.ToString();
+                    break;
+                case ButtonType.Amount:
+                    buttonLevel.text = factoryBase.AmountNum.ToString();
+                    break;
+                case ButtonType.Speed:
+                    buttonLevel.text = factoryBase.SecSpeedNum.ToString();
+                    break;
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (buttonName == null || buttonCost == null || buttonLevel == null) return;
+        if (buttonCost != null)
+        {
+            switch (buttonType)
+            {
+                case ButtonType.Point:
+                    buttonCost.text = factoryBase.GetPointCost().ToReadableString();
+                    break;
+                case ButtonType.Amount:
+                    buttonCost.text = factoryBase.GetAmountCost().ToReadableString();
+                    break;
+                case ButtonType.Speed:
+                    buttonCost.text = factoryBase.GetSpeedCost().ToReadableString();
+                    break;
+            }
+        }
+        if (buttonLevel != null)
+        {
+            switch (buttonType)
+            {
+                case ButtonType.Point:
+                    buttonLevel.text = factoryBase.PointNum.ToString();
+                    break;
+                case ButtonType.Amount:
+                    buttonLevel.text = factoryBase.AmountNum.ToString();
+                    break;
+                case ButtonType.Speed:
+                    buttonLevel.text = factoryBase.SecSpeedNum.ToString();
+                    break;
+            }
+        }
+    }
 
-        AddFactoryBase factory = GetComponent<AddFactoryBase>();
-        if (factory == null) return;
-
-        buttonName.text = factory.Name;
-        buttonCost.text = factory.GetBuyCost(1).ToString("F0");
-        buttonLevel.text = "Lv." + factory.Level.ToString();
-
-        // ボタンの有効/無効を設定
-        //GetComponent<Button>().interactable = factory.IsBuy(1);
-
+    public void OnClickButton()
+    {
+        switch (buttonType)
+        {
+            case ButtonType.Point:
+                if (factoryBase.IsPointBuy())
+                {
+                    factoryBase.PointBuy();
+                }   
+                break;
+            case ButtonType.Amount:
+                if(factoryBase.IsAmountBuy())
+                {
+                    factoryBase.AmountBuy();
+                }
+                break;
+            case ButtonType.Speed:
+                int level = factoryBase.SecSpeedNum;
+                if (level >= 9) break;
+                if (factoryBase.IsSpeedBuy())
+                {
+                    factoryBase.SpeedBuy();
+                }
+                break;
+        }
     }
 }
